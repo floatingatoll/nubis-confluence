119eb1235e9eac59b1c6b7bcd15ac51c468296fc
limed <limed@mozilla.com>
2015-06-17 16:31:20 -0700

    Remove consul data once we are done with the stack

--- bin/delete.sh
+++ bin/delete.sh
@@ -9,9 +9,15 @@ PROJECT_DIR=${BASE_DIR}/nubis-confluence
 # Name of the stack, only required value here
 STACK_NAME=$1
 
+export PATH="~/git/nubis-builder/bin:$PATH"
+
 if [[ -z ${STACK_NAME} ]]; then
     echo "Usage: $0 <stack name>"
     exit 1
 fi
 
+echo "Deleting cloudformation stack: ${STACK_NAME}"
 aws cloudformation delete-stack --stack-name ${STACK_NAME}
+
+echo "Deleting consul data from stack: ${STACKNAME}"
+nubis-consul --settings ${PROJECT_DIR}/nubis/cloudformation/parameters.json --stack-name ${STACKNAME} delete
